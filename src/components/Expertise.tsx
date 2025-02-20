d7b210166c08999cabf39b00a38bfb421e7bdf85
BackEndTeamXNL <team.back@nelixair.com>
2025-02-20 06:03:25 +0100

    expertise icons change

--- src/components/Expertise.tsx
+++ src/components/Expertise.tsx
@@ -1,9 +1,10 @@
 import React from "react";
 import '@fortawesome/free-regular-svg-icons'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faReact, faDocker, faPython } from '@fortawesome/free-brands-svg-icons';
+import { faDocker, faDev } from '@fortawesome/free-brands-svg-icons';
 import Chip from '@mui/material/Chip';
 import '../assets/styles/Expertise.scss';
+import { faCloud, faCode, faInfinity, faLaptopCode } from "@fortawesome/free-solid-svg-icons";
 
 
 const labelsFirst = [
@@ -59,7 +60,7 @@ function Expertise() {
             <h1>Expertise</h1>
             <div className="skills-grid">
                 <div className="skill">
-                    <FontAwesomeIcon icon={faReact} size="3x"/>
+                <FontAwesomeIcon icon={faInfinity} size="3x"/>
                     <h3>DevOps & Automation</h3>
                     <p>I have extensive experience in setting up CI/CD pipelines, automating infrastructure provisioning, and managing cloud environments using tools like Docker, Kubernetes, Ansible and Terraform.</p>
                     <div className="flex-chips">
@@ -71,7 +72,7 @@ function Expertise() {
                 </div>
 
                 <div className="skill">
-                    <FontAwesomeIcon icon={faDocker} size="3x"/>
+                    <FontAwesomeIcon icon={faLaptopCode} size="3x"/>
                     <h3>Backend & Cloud Development</h3>
                     <p>I specialize in designing and developing scalable backend systems and cloud-based solutions using modern technologies like Java, Spring Boot, and Microservices.</p>
                     <div className="flex-chips">
@@ -83,7 +84,7 @@ function Expertise() {
                 </div>
 
                 <div className="skill">
-                    <FontAwesomeIcon icon={faPython} size="3x"/>
+                    <FontAwesomeIcon icon={faCloud} size="3x"/>
                     <h3>Cloud & Infrastructure</h3>
                     <p>I help organizations optimize their cloud infrastructure, implement security best practices, and ensure high availability and scalability of their systems.</p>
                     <div className="flex-chips">
